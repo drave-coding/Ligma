8d781e2aed6e2d0847b6e687456b00074da81cfa
Abhishek Das <abhishek2022.work@gmail.com>
2025-01-12 11:56:25 +0530

    base setup

---
+++ app/Room.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+    const publicApiKey = process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY;
+
+  if (!publicApiKey) {
+    throw new Error("NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY is not defined");
+  }
+  return (
+    <LiveblocksProvider publicApiKey={publicApiKey}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
